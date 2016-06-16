634f377b2996c9ebca363a75a44969bb05ba5e30
Alek Shnayder <noradbase@gmail.com>
2016-06-15 20:35:14 -0500

    move javascript out into its own .js file

---
+++ main.js
@@ -0,0 +1,7 @@
+	$(document).ready(function(){
+		
+		//Fade in three sections once page is loaded
+		$('#Projects').delay(200).fadeIn(700);
+		$('#Current').delay(400).fadeIn(700);
+		$('#Previous').delay(600).fadeIn(700);
+	});
