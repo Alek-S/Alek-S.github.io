207e3a9e1fe1526dfc6d0fb7510e3db110a1ab52
Alek Shnayder <noradbase@gmail.com>
2017-01-08 20:45:31 -0600

    content boxes style, content style, organization

--- main.js
+++ main.js
@@ -3,5 +3,5 @@
 		//Fade in three sections once page is loaded
 		$('#Projects').delay(200).fadeIn(700);
 		$('#Current').delay(400).fadeIn(700);
-		$('#Previous').delay(600).fadeIn(700);
+		$('#skills').delay(600).fadeIn(700);
 	});
