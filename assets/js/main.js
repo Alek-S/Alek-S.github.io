64dd012879f298556427d30f6a445511a39339d0
Alek Shnayder <alek.shnayder@gmail.com>
2017-05-07 13:22:30 -0500

    Minor: Project descriptions

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 		
 		//Fade in three sections once page is loaded
 		$('#Summary').delay(200).fadeIn(700);
-		$('#skills').delay(400).fadeIn(700);
+		$('#Skills').delay(400).fadeIn(700);
 		$('#Projects').delay(600).fadeIn(700);
 		$('#Current').delay(800).fadeIn(700);
 	});
