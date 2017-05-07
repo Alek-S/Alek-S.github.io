e3adbfacca8c0d6503cf67e8735d609fecf8ddc2
Alek Shnayder <alek.shnayder@gmail.com>
2017-05-07 10:44:19 -0500

    Summary section, profile icons, orgarnization

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+	$(document).ready(function(){
+		
+		//Fade in three sections once page is loaded
+		$('#Summary').delay(200).fadeIn(700);
+		$('#skills').delay(400).fadeIn(700);
+		$('#Projects').delay(600).fadeIn(700);
+		$('#Current').delay(800).fadeIn(700);
+	});
--- main.js
+++
@@ -1,7 +0,0 @@
-	$(document).ready(function(){
-		
-		//Fade in three sections once page is loaded
-		$('#Projects').delay(200).fadeIn(700);
-		$('#Current').delay(400).fadeIn(700);
-		$('#skills').delay(600).fadeIn(700);
-	});
