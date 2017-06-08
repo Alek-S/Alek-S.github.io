64acb58980a3ebc48654a0e10ffe7b8948699cec
Alek Shnayder <alek.shnayder@gmail.com>
2017-06-08 16:43:35 -0500

    Add view buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,16 @@
 		//Fade in sections once DOM is ready
 		$('#Skills').delay(delay).fadeIn(fadeLength);
 		$('#Articles').delay(delay * 2).fadeIn(fadeLength);
-		$('#Projects').delay(delay * 3).fadeIn(fadeLength);
-		$('#Current').delay(delay * 4).fadeIn(fadeLength);
+		$('#Current').delay(delay * 3).fadeIn(fadeLength);
 
 		$('.moreBtn').on('click', function(){
 			$(this).toggleClass('expanded');
 			$(this).siblings('.moreSection').slideToggle();
 
 			if( $(this).hasClass('expanded') ){
-				$(this).html('<a>Show Less</a>');
+				$(this).html('Show Less');
 			}else{
-				$(this).html('<a>Show More &#9656;</a>');
+				$(this).html('Show More');
 			}
 		});
 	});
