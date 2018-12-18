b656a82fc8737dfa4ae42adb46f60f7076b90a50
Alek Shnayder <alek.shnayder@gmail.com>
2018-12-18 13:17:33 -0600

    Overview and projects fade in via css instead of jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,94 +1,84 @@
-$(document).ready(function(){
-
-	//====DOCUMENT LOADED====
-
-	//DOM cache
-	const $overview = $('#overview');
-	const $skills = $('#Skills');
-	const $articles = $('#Articles');
-	const $current = $('#Current');
-	const $projectsSection = $('#Projects');
-
-	let projectView = document.getElementById('projectView');
-	let projects = document.getElementById('Projects');
-	let overview = document.getElementById('overview');
-	let overviewSection = document.getElementsByClassName('overviewSection');
-	let projectsSection = document.getElementsByClassName('projectsSection');
+//DOM cache
+const projectView = document.getElementById('projectView');
+const projects = document.getElementById('Projects');
+const overview = document.getElementById('overview');
+const overviewSection = document.getElementsByClassName('overviewSection');
+const projectsSection = document.getElementsByClassName('projectsSection');
+const skills = document.getElementById('Skills');
+const articles = document.getElementById('Articles');
+const current = document.getElementById('Current');
 
 
-	//====CLICK EVENTS====
 
-	//show more for work section
-	$('.moreBtn').on('click', function(){
-		$(this).toggleClass('expanded');
-		$(this).siblings('.moreSection').slideToggle();
+//Fade in overview sections - true/false for 200ms delay
+const overviewFadeIn = () => {
+	const delayTime = 200;
 
-		if( $(this).hasClass('expanded') ){
-			$(this).html('Show Less');
-		}else{
-			$(this).html('Show More');
-		}
-	});
-
-	//switch to project view when clicked
-	projectView.addEventListener('click', event => {
-		event.preventDefault();
-
-		//update nav buttons
-		overview.classList.remove("active");
-		overview.classList.add("notActive");
-
-		projectView.classList.remove("notActive");
-		projectView.classList.add("active");
-
-		for (let i = 0; i < overviewSection.length; i++) {
-			overviewSection[i].style.display = 'none';
-		}
-
-		projects.style.display = 'block';
-
-	});
-
-
-	//switch to overview when clicked
-	overview.addEventListener('click', event => {
-		event.preventDefault();
-
-		//update nav buttons
-		projectView.classList.remove("active");
-		projectView.classList.add("notActive");
-
-		overview.classList.remove("notActive");
-		overview.classList.add("active");
-
-		// hide projects component
-		$projectsSection.hide();
-
-		for (let i = 0; i < projectsSection.length; i++) {
-			projectsSection[i].style.display = 'none';
-		}
-
-		// show overview section
-		overviewFadeIn(false);
-	});
-		
-
-	//====FUNCTIONS====
-
-	//Fade in overview sections - true/false for 200ms delay
-	function overviewFadeIn(delay){
-		const delayTime = 200;
-		const fadeLength = 700;
-
-		if(delay === true){
-			$skills.delay(delayTime).fadeIn(fadeLength);
-			$articles.delay(delayTime * 2).fadeIn(fadeLength);
-			$current.delay(delayTime * 3).fadeIn(fadeLength);
-		}else{
-			$skills.fadeIn(fadeLength);
-			$articles.delay(delayTime).fadeIn(fadeLength);
-			$current.delay(delayTime * 2).fadeIn(fadeLength);
-		}
+	for (let i = 0; i < overviewSection.length; i++) {
+		overviewSection[i].style.display = 'block';
 	}
 
+	setTimeout(() => { skills.style.opacity = 1 }, delayTime);
+	setTimeout(() => { articles.style.opacity = 1 }, delayTime * 2);
+	setTimeout(() => { current.style.opacity = 1 }, delayTime * 3);
+}
+
+
+
+//====CLICK EVENTS====
+
+//show more for work section
+$('.moreBtn').on('click', function(){
+	$(this).toggleClass('expanded');
+	$(this).siblings('.moreSection').slideToggle();
+
+	if( $(this).hasClass('expanded') ){
+		$(this).html('Show Less');
+	}else{
+		$(this).html('Show More');
+	}
+});
+
+
+
+//switch to project view when clicked
+projectView.addEventListener('click', event => {
+	event.preventDefault();
+
+	//update nav buttons
+	overview.classList.remove("active");
+	overview.classList.add("notActive");
+
+	projectView.classList.remove("notActive");
+	projectView.classList.add("active");
+
+	// hide overview and show projects
+	for (let i = 0; i < overviewSection.length; i++) {
+		overviewSection[i].style.display = 'none';
+		overviewSection[i].style.opacity = 0;
+	}
+
+	projects.style.display = 'block';
+	setTimeout(() => { projects.style.opacity = 1 }, 100);
+});
+
+
+
+
+//switch to overview when clicked
+overview.addEventListener('click', event => {
+	event.preventDefault();
+
+	//update nav buttons
+	projectView.classList.remove("active");
+	projectView.classList.add("notActive");
+
+	overview.classList.remove("notActive");
+	overview.classList.add("active");
+
+	// hide projects component and show overview
+	projects.style.display = 'none';
+	projects.style.opacity = 0;
+
+	overviewFadeIn();
 });
